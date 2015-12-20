cdaeaa825db191bd65aad3aaaeb3178738727f05
Peter Williams <peter@newton.cx>
2015-12-19 21:07:37 -0500

    src/eventloop.cpp: use portable method for initializing recursive mutex

--- src/eventloop.cpp
+++ src/eventloop.cpp
@@ -85,8 +85,11 @@ DefaultMutex::DefaultMutex(bool recursive)
 {
   if (recursive)
   {
-    pthread_mutex_t recmutex = PTHREAD_RECURSIVE_MUTEX_INITIALIZER_NP;
-    _mutex = recmutex;
+    pthread_mutexattr_t attr;
+
+    pthread_mutexattr_init(&attr);
+    pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_RECURSIVE);
+    pthread_mutex_init(&_mutex, &attr);
   }
   else
   {
